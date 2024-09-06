ba3b45721f89797d78d9b9b2a810adcc88cd2033
Jayasri Jallipalli <103980907+jaya3006@users.noreply.github.com>
2024-09-06 12:09:42 +0530

    Update and rename HelloWorld to Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Calculator{
+    public static void main(String[] args) {
+        int num1 = 10; // First number
+        int num2 = 20; // Second number
+        int sum = num1 + num2; // Adding the two numbers
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
